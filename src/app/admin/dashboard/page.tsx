9420fd67941acff18de9ae0ae96d2920d611f689
anamkc <anamkhatri10@gmail.com>
2023-10-06 09:14:02 +0545

    fetchinguser details in admin

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,30 +1,32 @@
-'use client'
+"use client";
 import { getAllBlocks } from "@/services/getAllBlocks.service";
 import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
-  const [blockData, setBlockData] = useState();
+  const [blockData, setBlockData] = useState<any>();
+  
   useEffect(() => {
     const fetchData = async () => {
       try {
         const data = await getAllBlocks();
-        console.log(data.allBlockData)
-        setBlockData(JSON.parse(data.allBlockData));
+        console.log(data.allBlockData);
+        setBlockData(data.allBlockData);
       } catch (err) {
         console.log("Error in Dashboard:", err);
       }
     };
     fetchData();
   }, []);
-
+  
   return (
     <div>
       <div>
         <div className="bg-black text-white h-screen relative overflow-hidden">
           <div className=" mt-[160px]">
-            {blockData && blockData.map(data => {
-              return data;
-            })}
+            {blockData &&
+              blockData.map((data) => {
+                return <div key={data.id}>{data.address}</div>;
+              })}
           </div>
         </div>
       </div>
--- src/services/getAllBlocks.service.ts
+++ src/services/getAllBlocks.service.ts
@@ -4,7 +4,9 @@ import { httpClientAdmin } from "@/config/httpClientAdmin";
 export const getAllBlocks = async () => {
   try {
     const res = await httpClientAdmin.get("/getallblocks");
+    console.log("response from getall blocks" + res.data);
     return res.data;
+    
   } catch (err) {
     console.error("Error fetching data:", err);
     throw err;
